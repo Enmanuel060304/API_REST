b8b3cd7356052d522a4d9b35572c2f21934637cc
Enmanuel Urbina <enmanuelu100@gmail.com>
2025-07-08 16:42:27 -0600

    deploy front end

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3001
-const cors = require('cors')
+const PORT = process.env.PORT || 3001;
+const cors = require("cors");
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
+app.use(express.static("dist"));
 
 let persons = [
   {
